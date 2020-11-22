e2871869fd9784268feb0588568051c6eeb91391
xuecqcn <33610948+xuecqcn@users.noreply.github.com>
2020-11-22 18:39:23 +0800

    Update 1907.sh

--- 1907.sh
+++ 1907.sh
@@ -13,7 +13,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' ./package/base-files/files/bin/config_gener
 
 
 # 修改主机名字，把Phicomm-K3修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/Phicomm-K3/g' ./package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/Phicomm-K3/g' ./package/base-files/files/bin/config_generate
 
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /xuecqcn build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
